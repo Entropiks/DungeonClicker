a5ed3ed860afcbfe30f4d1b3629c8a944d7ed2c4
entropiks <mikecorseri@gmail.com>
2022-01-11 21:57:05 -0800

    back in CA, scaffolding to make this game a game this week

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -7,7 +7,7 @@ function Inventory() {
         <li name="copper" cost="20" amount={amount}></li>
         <p>Copper ore (2)</p>
         <p>Iron ore (4)</p>
-
+    {/* somehing */}
       </div>
     </div>
   )
